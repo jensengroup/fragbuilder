4a2132eba4a49dacebf284d67f85b775276bf445
Anders Steen Christensen <andersx@nano.ku.dk>
2013-12-06 21:06:15 +0100

    Updated an example demonstating the api

--- examples/make_peptide.py
+++ examples/make_peptide.py
@@ -2,44 +2,31 @@ import fragbuilder
 import string
 import math
 
-sequence = "YDG"
+sequence = "GKG"
 
-my_peptide = fragbuilder.peptide(sequence)
+my_peptide = fragbuilder.peptide(sequence, nterm="methyl", cterm="methyl")
 
 print my_peptide.get_smiles()
-print "Charge:  ",  my_peptide.get_charge()
+print "Charge:  ", my_peptide.get_charge()
 print "Length:  ", my_peptide.get_length()
 print "Sequence:", my_peptide.get_sequence()
 
+my_peptide.set_residue_bb_angles(1, [ -156.3,  142.3])
+my_peptide.set_residue_bb_angles(2, [   48.8,   42.3])
+my_peptide.set_residue_bb_angles(3, [   81.3,    0.2])
 
-#ASPD11 angles
+print my_peptide.sample_residue_bb_angles(2)
+print my_peptide.get_residue_bb_angles(2)
+print my_peptide.sample_residue_chi_angles(2)
+print my_peptide.get_residue_chi_angles(2)
 
-phi_psi1    = [fragbuilder.to_rad( -156.3),
-               fragbuilder.to_rad( 142.3)]
+#my_peptide.set_residue_chi_angles(2, [  -50.6, -75.1])
 
-phi_psi2    = [fragbuilder.to_rad(48.8),
-               fragbuilder.to_rad(42.3)]
-
-phi_psi3    = [fragbuilder.to_rad(81.3),
-               fragbuilder.to_rad(0.2)]
-
-
-
-my_peptide.set_residue_bb_angles(1, phi_psi1)
-my_peptide.set_residue_bb_angles(2, phi_psi2)
-my_peptide.set_residue_bb_angles(3, phi_psi3)
-
-
-chi_angles = [fragbuilder.to_rad(-50.6),
-              fragbuilder.to_rad(-75.1)]  #OBS TJEK
-my_peptide.set_residue_chi_angles(2, chi_angles)
-
-my_peptide.regularize()
-
+print my_peptide.get_bb_angles()
 my_peptide.write_xyz("D101.xyz")
-my_peptide.write_g09_opt_file("my_opt_D101.com")
+#my_peptide.write_g09_opt_file("my_opt_D101.com")
 
-
+print my_peptide.__class__.__name__
 
 
 
