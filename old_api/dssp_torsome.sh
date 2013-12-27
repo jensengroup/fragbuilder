0e72b0dd6e02fb13c7b24199e4e43c8eb4955e2a
Anders Steen Christensen <andersx@nano.ku.dk>
2013-12-27 15:36:55 +0100

    Removed deprecated code bits.

--- old_api/dssp_torsome.sh
+++
@@ -1,1 +0,0 @@
-/home/andersx/programs/dsspcmbi -na tmp.pdb | grep -Ev "\.$" | grep -v "  #" | cut -b 17 | tr -d "\n" | sed -e "s/[^HE]/C/g" > tmp.sec
--- settings.py
+++
@@ -1,2 +0,0 @@
-direct_g09 = "/home/andersx/pbs/direct_g09.sh"
-fragbuild_dir = "/home/andersx/dev/fragbuilder/"
